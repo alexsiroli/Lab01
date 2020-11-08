43b99aa9267a09d0b2755fe102d1fc2d08042699
Alex Siroli <alex.siroli@studio.unibo.it>
2020-11-08 17:42:48 +0100

    03 - fatto

--- 03-complexNum-example/ComplexNum.java
+++ 03-complexNum-example/ComplexNum.java
@@ -1,37 +1,50 @@
 class ComplexNum {
 
-    /*
-     * Inserire qui la dichiarazione dei campi della classe
-     */
+    // Inserire qui la dichiarazione dei campi della classe
 
+    double re;	//parte reale
+    double im;	//parte immaginaria
+    
     void build(double re, double im) {
-        /*
-         * Inserire qui l'inizializzazione dei campi con i parametri forniti in
-         * input
-         *
-         */
+    	
+    	// Inserire qui l'inizializzazione dei campi con i parametri forniti in
+        // input
+        
+    	this.re = re;
+        this.im = im;
     }
 
     boolean equal(ComplexNum num) {
-        /*
-         * Implementare il metodo in modo che restituisca true se e solo se il
-         * numero complesso Ã¨ uguale al parametro num passato in input
-         */
-        return false;
+        
+        // Implementare il metodo in modo che restituisca true se e solo se il
+        // numero complesso è uguale al parametro num passato in input
+    
+        return this.re == num.re && this.im == num.im;
     }
 
     void add(ComplexNum num) {
-        /*
-         * Implementare il metodo in modo che venga aggiunto il numero complesso
-         * passato in input - Rif. Appendice A1 (slide)
-         */
+        
+        // Implementare il metodo in modo che venga aggiunto il numero complesso
+        // passato in input - Rif. Appendice A1 (slide)
+         
+    	this.re = this.re + num.re;
+        this.im = this.im + num.im;
     }
 
     String toStringRep() {
-        /*
-         * Implementare il metodo in modo che restituisca una rappresentazione
-         * testuale del numero complesso
-         */
-        return "";
+        
+        // Implementare il metodo in modo che restituisca una rappresentazione
+        // testuale del numero complesso
+         
+    	if (this.im > 0){
+            if (this.im == 1){
+                return this.re + "+" + "i";
+            }
+            return this.re + "+" + this.im + "i";
+        }
+        if (this.im == 0){
+             return "" + this.re;
+        }
+        return "" + this.re + this.im + "i";
     }
 }
