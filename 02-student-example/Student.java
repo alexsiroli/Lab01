c3f978a467bc85b99d42b4ae09c7fdf1ff9b6195
Alex Siroli <alex.siroli@studio.unibo.it>
2020-11-08 17:30:31 +0100

    02 - fatto

--- 02-student-example/Student.java
+++ 02-student-example/Student.java
@@ -1,14 +1,28 @@
 class Student {
 
     // ... Aggiungere qui la definizione dei campi
+	
+	String name;
+    String surname;
+	int id;
+    int matriculationYear;
 
-    void build(/* Aggiungere i parametri di input */) {
+    void build(String name, String surname, int id, int year) {
         // ... Inizializzazione dei campi della classe
+    	this.name = name;
+        this.surname = surname;
+        this.id = id;
+        this.matriculationYear = year;
+    	
     }
 
     void printStudentInfo() {
         /*
          * Aggiungere i comandi per la stampa delle informazioni sullo studente
          */
+    	System.out.println("Nome: " + this.name);
+        System.out.println("Cognome: " + this.surname);
+        System.out.println("Matricola: " + this.id);
+        System.out.println("Anno immatricolazione: " + this.matriculationYear);
     }
 }
--- 02-student-example/TestStudent.java
+++ 02-student-example/TestStudent.java
@@ -1,12 +1,15 @@
 class TestStudent {
   public static void main(String[] args) {
       Student student = new Student();
+      
       /*
        * aggiornare l'invocazione del metodo build con l'aggiunta dei
        * parametri
        */
-      student.build();
+      
+      student.build("Alex", "Siroli", 1001, 2019);
       student.printStudentInfo();
+      
       /*
        * Creare dei nuovi oggetti relativi agli studenti:
        *
@@ -16,5 +19,17 @@ class TestStudent {
        *
        * Stampare a video le informazioni relative a ciascuno studente.
        */
+      
+      Student student01 = new Student();
+      student01.build("Luigi", "Gentile", 1015, 2019);
+      student01.printStudentInfo();
+
+      Student student02 = new Student();
+      student02.build("Simone", "Bianchi", 1016, 2018);
+      student02.printStudentInfo();
+
+      Student student03 = new Student();
+      student03.build("Andrea", "Bracci", 1017, 2017);
+      student03.printStudentInfo();
   }
 }
